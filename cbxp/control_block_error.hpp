2d3983daeadb1595e989881a1661d45716d510a1
Elijah Swift <Elijah.Swift@IBM.com>
2026-01-26 10:56:55 -0500

    Explicit enum declarations
    Signed-off-by: Elijah Swift <Elijah.Swift@IBM.com>

--- cbxp/control_block_error.hpp
+++ cbxp/control_block_error.hpp
@@ -2,7 +2,7 @@
 #define __CONTROL_BLOCK_ERROR_H_
 
 namespace CBXP {
-enum Error { BadControlBlock = 1, BadInclude, BadFilter };
+enum Error { BadControlBlock = 1, BadInclude = 2, BadFilter = 3 };
 class CBXPError : public std::exception {
  private:
   Error error_code_;
