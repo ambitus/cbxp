8d063a29ad7bfef2377d82074d97d28a297ad1ba
varunchennamadhava <varunchennamadhava@gmail.com>
2026-01-27 14:26:38 -0500

    fixing issue 2
    Signed-off-by: varunchennamadhava <varunchennamadhava@gmail.com>

--- cbxp/control_blocks/asxb.hpp
+++ cbxp/control_blocks/asxb.hpp
@@ -9,7 +9,7 @@ class ASXB : public ControlBlock {
  public:
   nlohmann::json get(void* __ptr32 p_control_block = nullptr) override;
   explicit ASXB(const std::vector<std::string>& includes)
-      : ControlBlock("assb", {}, includes) {}
+      : ControlBlock("asxb", {}, includes) {}
 };
 
 }  // namespace CBXP
