9f74585747ea8e67456d89529adc2bac480ecb19
Elijah Swift <Elijah.Swift@IBM.com>
2026-05-15 16:05:53 -0400

    PR Comments #6.1 (still needs testing)
    Signed-off-by: Elijah Swift <Elijah.Swift@IBM.com>

--- cbxp/control_blocks/ascb.cpp
+++ cbxp/control_blocks/ascb.cpp
@@ -61,12 +61,12 @@ nlohmann::json ASCB::get(const void* p_control_block,
 
   for (const auto& [include, cbxp_options] : options_map_) {
     if (include == "assb") {
-      ascb_json["ascbassb"] = CBXP::ASSB(cbxp_options_).get(p_ascb->ascbassb);
+      ascb_json["ascbassb"] = CBXP::ASSB(cbxp_options).get(p_ascb->ascbassb);
       if (ascb_json["ascbassb"].is_null()) {
         return {};
       }
     } else if (include == "oucb") {
-      ascb_json["ascboucb"] = CBXP::OUCB(cbxp_options_).get(p_ascb->ascboucb);
+      ascb_json["ascboucb"] = CBXP::OUCB(cbxp_options).get(p_ascb->ascboucb);
       if (ascb_json["ascboucb"].is_null()) {
         return {};
       }
