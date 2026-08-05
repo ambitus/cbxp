00b024a4ec70a3c491008e5c764c2a505a2023ab
Elijah Swift <Elijah.Swift@IBM.com>
2026-08-05 15:29:41 -0400

    tweaks just trying to get it working 13
    Signed-off-by: Elijah Swift <Elijah.Swift@IBM.com>

--- cbxp/control_block_explorer.hpp
+++ cbxp/control_block_explorer.hpp
@@ -19,6 +19,7 @@ class ControlBlockExplorer {
   std::string control_block_operation_ = "";
 
   static std::unordered_map<std::string, ControlBlock> control_blocks_;
+  static bool control_blocks_init_failed_;
   static std::string mapToString(
       const std::unordered_map<std::string, ControlBlock>& map);
   static std::unordered_map<std::string, ControlBlock> loadCustomControlBlocks(
